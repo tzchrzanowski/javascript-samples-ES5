2620666fdb9cc5918932edb682c0d4b6d7f2b7dc
Tom <tzchrzanowski@gmail.com>
2017-05-14 21:02:51 +0200

    Add files via upload

---
+++ promise example/promise-then-catch.js
@@ -0,0 +1,27 @@
+var promise = new Promise(function(resolve, reject) {
+    //do stuff
+    var isSuccessful = true;
+    setTimeout(function(){ //asynchronously process after 5 seconds
+      if (isSuccessful) { //if everything is successful
+          resolve('success!');
+      }
+      else{               //if something went wrong
+          reject(Error("failure."))
+      }
+
+    },5000);
+
+
+});
+
+//promise status changes from pending to resolved after 5 seconds
+
+promise.then(function(val){//val represents the fulfillment value
+
+    console.log(val);//logs "success!" since promise resolved
+
+}).catch(function(val){//val represents the rejection value
+
+    console.log(val); //doesn't occur since promise never rejected
+
+});
