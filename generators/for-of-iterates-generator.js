3bb7aeb05fe420f675be294ab1b8112ef352168b
Tom <tzchrzanowski@gmail.com>
2017-06-09 21:42:09 +0200

    Add files via upload

---
+++ generators/for-of-iterates-generator.js
@@ -0,0 +1,21 @@
+function* genFunc(){
+    yield 'a';
+    yield;  
+    yield* [1,2,3];
+    yield 123;
+
+    return "finished";
+
+}
+
+for (var x of genFunc()){ //for...of statement
+    console.log(x); 
+}
+//Outputs:
+//'a'
+// undefined
+// 1
+// 2
+// 3
+// 123
+// <-- return value is not outputted
